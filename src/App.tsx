f90b5e0ed51382105236e63596d11cca8b11e8c0
MyakotiVydry <demiana13@mail.ru>
2026-09-23 21:58:29 +0500

    feat: ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import { Heading, Box, Container, TextField } from "@radix-ui/themes";
-import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { Container } from "@radix-ui/themes";
 
-import { Books } from "./views/layout/books";
+import { Books, Header, Filters } from "@views/layout";
 
 const App = () => {
 
   return (
     <Container>
-        <Box>
-            <Heading>My Library Front</Heading>
-        </Box>
-        <Box>
-        <TextField.Root placeholder="Find book by author">
-            <TextField.Slot>
-                <MagnifyingGlassIcon height="16" width="16" />
-            </TextField.Slot>
-        </TextField.Root>
-        </Box>
-
+        <Header/>
+        <Filters/>
         <Books/>
     </Container>
   )
--- src/views/layout/books/book/index.tsx
+++ src/views/layout/books/book/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Heading, Card } from "@radix-ui/themes";
+import { Heading, Card, Flex, Text, Box, Badge } from "@radix-ui/themes";
+
 import type { Book } from "@data/book";
 
 interface Props {
@@ -8,21 +9,47 @@ interface Props {
 
 export const Item: React.FC<Props> = ({ data }: Props) => {
     const {
-        id,
         author,
         completed,
-        completed_at,
-        created_at,
         pages,
         published_year,
         review,
         title,
     } = data
 
-    return (
-        <Card>
-            <Heading size="4">{title}, {author}</Heading>
+    const statusText = completed ? 'Завершено' : 'Не завершено';
+    const statusColor = completed ? 'green' : 'yellow';
 
+    return (
+        <Card size="2" variant="surface">
+
+            <Heading size="4">{title}, {author}</Heading>
+            <Flex direction="column" gap="2">
+                <Box>
+                    <Text>{"Наименование: "}</Text>
+                    <Text weight="bold">{title}</Text>
+                </Box>
+                <Box>
+                    <Text>{"Автор: "}</Text>
+                    <Text weight="bold">{author}</Text>
+                </Box>
+                <Box>
+                    <Text>{"Кол-во страниц: "}</Text>
+                    <Text weight="bold">{pages}</Text>
+                </Box>
+                <Box>
+                    <Text>{"Год публикации: "}</Text>
+                    <Text weight="bold">{published_year}</Text>
+                </Box>
+                <Box>
+                    <Text>{"Статус: "}</Text>
+                    <Badge color={statusColor}>{statusText}</Badge>
+                </Box>
+                <Box>
+                    <Text>{"Ревью: "}</Text>
+                    <Text weight="bold">{review || "-"}</Text>
+                </Box>
+            </Flex>
         </Card>
     )
 }
